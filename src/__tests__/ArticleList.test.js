9a5e181e027d052b280e591571b2e2e8d6dd5364
brnomosa <omosabria99@gmail.com>
2024-05-20 11:49:57 +0300

    complete

--- src/__tests__/ArticleList.test.js
+++ src/__tests__/ArticleList.test.js
@@ -1,34 +1,15 @@
 import "@testing-library/jest-dom";
-import { render } from "@testing-library/react";
-import ArticleList from "../components/ArticleList";
+import { render, screen } from "@testing-library/react";
+import Header from "../components/Header";
 
-const posts = [
-  {
-    id: 1,
-    title: "Components 101",
-    date: "December 15, 2020",
-    preview: "Setting up the building blocks of your site",
-  },
-  {
-    id: 2,
-    title: "React Data Flow",
-    date: "December 11, 2020",
-    preview: "Passing props is never passé",
-  },
-  {
-    id: 3,
-    title: "Function Components vs Class Components",
-    date: "December 10, 2020",
-    preview: "React, meet OOJS.",
-  },
-];
-
-test("renders a <main> element", () => {
-  const { container } = render(<ArticleList posts={posts} />);
-  expect(container.querySelector("main")).toBeInTheDocument();
+test("renders a <header> element", () => {
+  const { container } = render(<Header />);
+  expect(container.querySelector("header")).toBeInTheDocument();
 });
 
-test("renders a Article component for each post passed as a prop", () => {
-  const { container } = render(<ArticleList posts={posts} />);
-  expect(container.querySelector("main").children).toHaveLength(3);
+test("renders a <h1> with the blog name", () => {
+  render(<Header name="Underreacted" />);
+  const h1 = screen.queryByText("Underreacted");
+  expect(h1).toBeInTheDocument();
+  expect(h1.tagName).toBe("H1");
 });
